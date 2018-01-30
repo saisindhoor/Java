60ff2f6e3f57ccbfc8f1fc6b965001d8272c15ae
saisindhoor <sindhoor25@gmail.com>
2018-01-30 11:05:33 -0500

    new

---
+++ Application/src/problem.java
@@ -0,0 +1,13 @@
+
+public class problem {
+public static void main(String[]args) {
+	int b;
+try {	
+	b=10/0;}
+catch(Exception e ){
+	
+	System.out.println("exception handled");
+}
+	System.out.println("ourside");
+}
+}
