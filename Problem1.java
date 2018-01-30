b58f34ec02b9cd335adee388bf69624f2cf20a9f
saisindhoor <sindhoor25@gmail.com>
2018-01-30 11:09:41 -0500

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,22 @@
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		int arr[]=new int[2];
+		arr[0]=5;
+		arr[1]=2;
+		try {
+		arr[2]=3;
+		
+		}
+		catch(Exception e) {
+			System.out.println("ok" );
+			
+		}
+		
+		//try {}
+		//catch(Exception e) {}
+
+	}
+
+}
